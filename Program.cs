367def649fa853796e485f7203ec0cbf9b72d33d
sheltongeorge <128258667+sheltongeorge@users.noreply.github.com>
2025-01-27 14:01:46 -0700

    tiktak.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Mission4Assignment;
+
+
+
+TikTak board = new TikTak();
+
+string[] input = { "1", "2", "3", "4", "X", "O", "7", "8", "9" };
+
+board.GameBoard(input);
+
+TikTak.CheckWinner(input, out string winner);
+
+Console.WriteLine($"Player {winner} wins!");
+
+
---
+++ TikTak.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+using System.Reflection;
+
+namespace Mission4Assignment
+{
+    internal class TikTak
+    {
+        //2) The supporting class (with whichever name you choose) will:
+        //• Receive the “board” array from the driver class
+        //• Contain a method that prints the board based on the information passed to it
+        //• Contain a method that receives the game board array as input and returns if there is a
+        //winner and who it was
+
+
+        public void GameBoard(string[] board)
+        {
+            Console.WriteLine(board[0] + " " + board[1] + " " + board[2]);
+            Console.WriteLine(board[3] + " " + board[4] + " " + board[5]);
+            Console.WriteLine(board[6] + " " + board[7] + " " + board[8]);
+        }
+
+        public static bool CheckWinner(string[] board, out string winner)
+        {
+            // Horizontal check
+            for (int i = 0; i < 9; i++)
+            {
+                if (board[i] == board[i + 1] && board[i + 1] == board[i + 2])
+                {
+                    winner = board[i];
+                    return true;
+                }
+
+                i += 3;
+            }
+            // Vertical check
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[0 + i] == board[3 + i] && board[0 + i] == board[6 + i ])
+                {
+                    winner = board[i];
+                    return true;
+                }
+            }
+
+            // Diagnols check
+            if (board[0] == board[4] && board[4] == board[8])
+            {
+                winner = board[0];
+                return true;
+            }
+
+            if (board[2] == board[4] && board[4] == board[6])
+            {
+                winner = board[2];
+                return true;
+            }
+
+            winner = " ";
+            return false;
+        }
+    }
+}
