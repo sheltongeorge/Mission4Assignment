a7919f893b25c582927656f52a6c52038bb4543d
randengrims <randengrimshaw@gmail.com>
2025-01-27 13:36:42 -0700

    changes

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 {
     private static void Main(string[] args)
     {
+        string choice = "";
         bool winner = false;
         List<char> numbers = new List<char> {'1','2','3','4','5','6','7','8','9'};
 
@@ -13,9 +14,11 @@
             // Print the game board
 
             // Determine Current Player
+            int currentPLayer = 1;
 
+            Console.WriteLine($"It is Player {currentPLayer}'s turn. Where would you like to place your marker?");
             // Validate the Input
-
+           
             // Update GameBoard
 
             // Check for a winner
@@ -25,6 +28,8 @@
             // Alternate Players
 
             // Code block to be executed
+
+            
         } while (!winner);
     }
 
